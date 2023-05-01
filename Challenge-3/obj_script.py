0fc458056c2c68c3646259e0647ec6a52442c417
Rakesh Yadav <50994371+raakesh593812@users.noreply.github.com>
2023-05-01 13:12:02 +0530

    Update obj_script.py

--- Challenge-3/obj_script.py
+++ Challenge-3/obj_script.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python
-
-import logging
-
 def get_value(value, key):
     keys = key.split('/')
     for key in keys:
@@ -11,12 +8,3 @@ def get_value(value, key):
         except BaseException as exception:
             logging.error(f"Exception Name: {type(exception).__name__}")
     return prt_value
-
-object = {'x': {'y': {'z': 'a'}}}
-key = 'x/y/z'
-
-testobj = {'a':{'b':{'c':'d'}}} 
-testkey = 'a/b/c'
-
-print("Value for object" + str(object) + "and key " + key + " is :", get_value(object, key))
-print("Value for object" + str(testobj) + "and key " + testkey + " is :", get_value(testobj, testkey))
