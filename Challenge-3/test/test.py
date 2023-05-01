bc418f457b4374218f13ac39106b16c7d410b500
Rakesh Yadav <50994371+raakesh593812@users.noreply.github.com>
2023-05-01 13:13:34 +0530

    Update test.py

--- Challenge-3/test/test.py
+++ Challenge-3/test/test.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python
-
+from obj_script import get_value
 import logging
-
-def get_value(value, key):
-    keys = key.split('/')
-    for key in keys:
-        try:
-            value = value.pop(key)
-            prt_value = value
-        except BaseException as exception:
-            logging.error(f"Exception Name: {type(exception).__name__}")
-    return prt_value
 
 object = {'x': {'y': {'z': 'a'}}}
 key = 'x/y/z'
